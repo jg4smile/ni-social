e755ad940f04f743e8aadac41f3c78eb1e22de5b
Wm. Dexter Jones <dexter.jones@vendaetta.com>
2017-09-17 13:31:15 -0400

    push of all files

---
+++ www/js/app/app.filters.js
@@ -0,0 +1,11 @@
+angular.module('northstar_social.app.filters', [])
+
+.filter('cleanUrl',function() {
+  return function(url) {
+    if (url) {
+      return url.replace('www.', '').replace('https://', '').replace('http://', '');
+    }
+  }
+})
+
+;
